05d7d6e30ce3d88c5fe5f08fa3cf95ce1cf7ebb0
Pavel Odvody <podvody@redhat.com>
2015-03-16 16:43:16 +0100

    Version bumps :(
    Signed-off-by: Pavel Odvody <podvody@redhat.com>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup, find_packages
 setup(
     name = 'libdoug',
-    version = '0.2.1',
+    version = '0.3',
     packages = find_packages(),
     scripts = ['doug-cli.py'],
     install_requires = ['requests<2.5.0', 'docker-py', 'clint'],
